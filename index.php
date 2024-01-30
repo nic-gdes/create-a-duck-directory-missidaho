847152adec24b92f46f47c65fe8ebe54f38d1a3f
Madeleine <mgleahy@nic.edu>
2024-01-30 15:34:31 -0800

    hero is working
    pics added for grid

--- index.php
+++ index.php
@@ -1,6 +1,45 @@
 <?php include('./components/header.php'); ?>
 <?php include('./components/nav.php'); ?>
 
+<div class="hero">
+    <img class="hero_image" src="./assets/images/advertising hero.jpg" alt="banner saying quackfest is coming to CDA">
+
+</div>
+
+<div class="grid_align">
+            <div class="grid_wrapper">
+
+                <div>
+                    <div class="grid_card">
+                        <div class="grid_content">
+                            <img src="./pics/Style and Charm 2.jpg">
+                            <h2>Favorite Movie</h2>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div>
+                    <div class="grid_card">
+                        <div class="grid_content">
+                            <img src="./pics/book.jpg">
+                            <h2>Favorite Hobbie</h2>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div>
+                    <div class="grid_card">
+                        <div class="grid_content">
+                            <img src="./pics/Art Print_ McNaughton's Jellyfish, 40x30in_.jpg">
+                            <h2>Favorite Place</h2>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
 
