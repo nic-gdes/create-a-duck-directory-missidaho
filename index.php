131d740279c0ef4d5d9b6bf38251b250b83ca616
Madeleine <mgleahy@nic.edu>
2024-01-30 19:11:37 -0800

    added grid

--- index.php
+++ index.php
@@ -12,8 +12,14 @@
                 <div>
                     <div class="grid_card">
                         <div class="grid_content">
-                            <img src="./pics/Style and Charm 2.jpg">
-                            <h2>Favorite Movie</h2>
+                            <img src="./assets/images/2BirdRed.png.webp">
+                            <h2>Rosie Red</h2>
+                            <h3>Favorite Foods</h3>
+                            <ul>
+                                <li>Strawberries</li>
+                                <li>Pepperoni</li>
+                                <li>Ketchup</li>
+                            </ul>
                         </div>
 
                     </div>
@@ -22,8 +28,14 @@
                 <div>
                     <div class="grid_card">
                         <div class="grid_content">
-                            <img src="./pics/book.jpg">
-                            <h2>Favorite Hobbie</h2>
+                            <img src="./assets/images/tzb1ugmtuuuvnngt03tw.jpg.webp">
+                            <h2>Duckie Cool</h2>
+                            <h3>Favorite Foods</h3>
+                            <ul>
+                                <li>Hot Dog</li>
+                                <li>Hot Dog</li>
+                                <li>Hot Dog</li>
+                            </ul>
                         </div>
 
                     </div>
@@ -32,8 +44,14 @@
                 <div>
                     <div class="grid_card">
                         <div class="grid_content">
-                            <img src="./pics/Art Print_ McNaughton's Jellyfish, 40x30in_.jpg">
-                            <h2>Favorite Place</h2>
+                            <img src="./assets/images/199060_600_600.jpg">
+                            <h2>Sarge</h2>
+                            <h3>Favorite Foods</h3>
+                            <ul>
+                                <li>Steak</li>
+                                <li>Potatoes</li>
+                                <li>Wiskey </li>
+                            </ul>
                         </div>
 
                     </div>
@@ -41,7 +59,7 @@
             </div>
         </div>
 
-
+        <?php include('./components/footer.php'); ?>
 
 <script src="./assets/js/script.js"></script>
 
