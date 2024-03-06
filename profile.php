47efb31bf63c947e3ae38725818d51e3590b8ba2
Madeleine <mgleahy@nic.edu>
2024-03-06 15:02:56 -0800

    Update profile.php

--- profile.php
+++ profile.php
@@ -2,44 +2,59 @@
 <?php include('./components/nav.php'); ?>
 <?php
 
-if(isset($_GET['id'])) {
+$duck_is_live = false;
+
+if (isset($_GET['id'])) {
     $id = htmlspecialchars($_GET['id']);
-    require ('./config/db.php');
+
+    require('./config/db.php');
 
     $sql = "SELECT id, name, favorite_foods, bio, img_src FROM ducks WHERE id=$id";
     $result = mysqli_query($conn, $sql);
 
     $duck = mysqli_fetch_assoc($result);
+
     mysqli_free_result($result);
     mysqli_close($conn);
+
+    // print_r($duck);
 }
 
 
 ?>
 
-<div class="profile_card">
-    <div class="profile_image_placeholder">
-        <!-- <img class="profile_image" src="./assets/images/2BirdRed.png.webp"> -->
-        <img class="profile_image" src="<?php echo $duck ['img_src'];  ?> alt=duck.">
-    </div>
-    <div class="flex_column">
-        <h2>
-            <?php echo $duck['name']?>
-        </h2>
-        <h3>
-        <?php echo $duck['favorite_foods']?>
-        </h3>
-        <?php $food_list = explode(",",)?>
-        <ul>
+<?php if ($duck_is_live) : ?>
+
+
+    <div class="profile_card">
+        <div class="profile_image_placeholder">
+            <!-- <img class="profile_image" src="./assets/images/2BirdRed.png.webp"> -->
+            <img class="profile_image" src="<?php echo $duck['img_src']; ?>">
+        </div>
+        <div class="flex_column">
+            <h2>
+                <?php echo $duck['name']; ?>
+            </h2>
+            <h3>
+                <?php echo $duck['favorite_foods'] ?>
+            </h3>
+            <?php $food_list = explode(",", $duck["favorite foods"]); ?>
+            <!-- <ul>
             <li>Strawberries</li>
             <li>Pepperoni</li>
             <li>Ketchup</li>
-        </ul>
-        <p>
-        <?php echo $duck['bio']?>
-        </p>
+        </ul> -->
+            <p>
+                <?php echo $duck['bio'] ?>
+            </p>
+        </div>
     </div>
-</div>
+
+<?php else : ?>
+    <section class="non-duck">
+        <h1>Sorry, this duck does not exist!</h1>
+    </section>
+<?php endif ?>
 
 
 <?php include('./components/footer.php'); ?>
